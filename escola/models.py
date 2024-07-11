c11682cb8b52a1504997bc913e5830b320837b42
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-10 23:57:24 -0300

    Criando arquivo gitignore e model para estudantes

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+'''
+Id
+Nome
+E-mail
+Não pode estar em branco
+CPF
+Máximo de 11 caracteres
+Data de Nascimento
+'''
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length= 100)
+    email = models.EmailField(blank= False, max_length= 30)
+    cpf = models.CharField(max_length= 11)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length= 14)
+
+    def __str__(self):
+        return self.nome
